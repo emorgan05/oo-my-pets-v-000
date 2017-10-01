9b4645de23cb742a44a44992bf1a90cb941f8f30
Emily Morgan <emorgan05@gmail.com>
2017-10-01 15:21:07 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def mood=
+    @mood = nervous
+  end
+
+  def mood
+    @mood
+  end
 end
