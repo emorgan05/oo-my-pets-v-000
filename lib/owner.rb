b61342c02fadaeda294b69300a7c120a05ab3c35
Emily Morgan <emorgan05@gmail.com>
2017-10-01 15:40:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,10 @@
 class Owner
   attr_accessor :name
   attr_reader :species
-  
+
   def initialize(species)
     @species = species
     @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
   end
 end
