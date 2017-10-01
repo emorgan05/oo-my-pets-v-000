5ba0ffdd6f4b790bef18e5927f6d93d90ef0f571
Emily Morgan <emorgan05@gmail.com>
2017-10-01 19:05:06 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,6 +1,5 @@
 class Fish
   attr_accessor :owner
-  attr_writer :mood
   attr_reader :name
 
   def initialize(name)
@@ -8,6 +7,10 @@ class Fish
     @mood = "nervous"
   end
 
+  def mood=(mood)
+    @mood = mood
+  end
+  
   def mood
     @mood
   end
